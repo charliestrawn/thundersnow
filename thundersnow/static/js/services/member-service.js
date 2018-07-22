29fe12a0be4c8aa0aa149af635747e432ceb218e
Charlie Strawn <cdstrawn@gmail.com>
2018-07-22 17:18:41 -0500

    add ability to consolidate members.

--- thundersnow/static/js/services/member-service.js
+++ thundersnow/static/js/services/member-service.js
@@ -3,11 +3,20 @@
 
     app.factory('memberService', ['$http', function ($http) {
         return {
-            getAll: function () {
+            getAll: function (withPayments) {
+                if (withPayments) {
+                    return $http.get('/api/members?with_payments=true');
+                }
                 return $http.get('/api/members');
             },
             getById: function (id) {
                 return $http.get('/api/members/' + id);
+            },
+            delete: function (id) {
+                return $http.delete('/api/members/' + id);
+            },
+            fixMember: function (id, new_id) {
+                return $http.put('/api/members/' + id, {'new_member_id': new_id});
             }
         };
     }]);
